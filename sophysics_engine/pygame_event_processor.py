588cfc9a12d51690a299e7ffaaafbf9f23f2d814
SophieSilver <87021304+SophieSilver@users.noreply.github.com>
2021-12-19 19:28:25 +0300

    Added an ability to "consume" pygame events.

--- sophysics_engine/pygame_event_processor.py
+++ sophysics_engine/pygame_event_processor.py
@@ -9,10 +9,28 @@ class PygameEvent(Event):
     """
     def __init__(self, event: pygame.event.Event):
         self.__event = event
+        self.__consumed = False
 
     @property
     def pygame_event(self) -> pygame.event.Event:
         return self.__event
+
+    @property
+    def consumed(self) -> bool:
+        """
+        Whether the event (such as a click) was consumed by a one of the listeners, which signals the other listeners
+        to ignore this event.
+
+        Useful when, for instance, you need an object to react when the user clicks it, and you don't want other
+        objects reacting to that click.
+        """
+        return self.__consumed
+
+    def consume(self):
+        """
+        Marks the event as consumed
+        """
+        self.__consumed = True
 
 
 class PygameEventProcessor(EnvironmentComponent):
