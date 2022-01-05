ac7a94e39104e40f8f302c84d4758814207c5419
SophieSilver <87021304+SophieSilver@users.noreply.github.com>
2022-01-05 14:33:24 +0300

    Added update ui method to ui panel component

--- sophysics_engine/ui_panel.py
+++ sophysics_engine/ui_panel.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from .simulation import EnvironmentComponent
+from .simulation import EnvironmentComponent, EnvironmentUpdateEvent
 from .gui_manager import GUIManager
 import pygame_gui
 
@@ -18,6 +18,8 @@ class GUIPanel(EnvironmentComponent):
         super().__init__()
 
     def setup(self):
+        self.environment.event_system.add_listener(EnvironmentUpdateEvent, self.__handle_update_event)
+
         self._ui_manager: GUIManager = self.environment.get_component(GUIManager)
         self._pygame_gui_manager = self._ui_manager.ui_manager
 
@@ -25,8 +27,20 @@ class GUIPanel(EnvironmentComponent):
 
         self._setup_ui()
 
+    def __handle_update_event(self, _: EnvironmentUpdateEvent):
+        self._update_ui()
+
     def _setup_ui(self):
         """
         method that actually sets up the ui
         """
         pass
+
+    def _update_ui(self):
+        """
+        Called every frame
+        """
+        pass
+
+    def _on_destroy(self):
+        self.environment.event_system.remove_listener(EnvironmentUpdateEvent, self.__handle_update_event)
