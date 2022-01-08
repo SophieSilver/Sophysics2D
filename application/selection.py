4443791262aee280e24d9132d9749a6fb9b2c244
SophieSilver <87021304+SophieSilver@users.noreply.github.com>
2022-01-08 15:44:33 +0300

    Added ability to drag the object around to BodyController

--- application/selection.py
+++ application/selection.py
@@ -4,9 +4,11 @@ Scripts that allow the user to select particular components
 from __future__ import annotations
 import pygame
 
-from sophysics_engine import Event
-from defaults import CircleClickable, GlobalClickable
+from sophysics_engine import Event, TimeSettings, RigidBody
+from defaults import CircleClickable, GlobalClickable, VectorArrowRenderer
 from typing import Optional
+
+from .select_renderer import SelectionRenderer
 
 
 class SelectionUpdateEvent(Event):
@@ -55,12 +57,36 @@ class BodyController(CircleClickable):
     def _clickable_start(self):
         self.__was_selected = False
         self.__global_selection: GlobalSelection = self.sim_object.environment.get_component(GlobalSelection)
+        self.__selection_renderer: SelectionRenderer = self.sim_object.get_component(SelectionRenderer)
+        self.__vector_renderer: VectorArrowRenderer = self.sim_object.get_component(VectorArrowRenderer)
+        self.__time_settings: TimeSettings = self.sim_object.environment.get_component(TimeSettings)
+
+        self.__mouse_offset_from_body: Optional[pygame.Vector2] = None
+
+        # commonly used by other scripts
+        self.__rigidbody: Optional[RigidBody] = self.sim_object.try_get_component(RigidBody)
 
         self.sim_object.environment.event_system.add_listener(SelectionUpdateEvent, self.__handle_selection_update)
 
     @property
+    def rigidbody(self) -> Optional[RigidBody]:
+        """
+        A reference to the object's rigidbody component. If the object doesn't have one,
+        the value is None.
+        """
+        return self.__rigidbody
+
+    @property
     def is_selected(self) -> bool:
         return self.__global_selection.selected_body is self
+
+    @property
+    def screen_position(self) -> pygame.Vector2:
+        return pygame.Vector2(self._camera.world_to_screen(self.sim_object.transform.position))
+
+    @screen_position.setter
+    def screen_position(self, value: pygame.Vector2):
+        self.sim_object.transform.position = self._camera.screen_to_world(value)
 
     def __handle_selection_update(self, event: SelectionUpdateEvent):
         selected_body = event.selected_body
@@ -73,6 +99,18 @@ class BodyController(CircleClickable):
             self.__was_selected = False
             self._on_deselect()
 
+    def _on_hold_start(self):
+        # record the relative position between the body and the mouse cursor
+        self.__mouse_offset_from_body = pygame.Vector2(pygame.mouse.get_pos()) - self.screen_position
+        # pause the simulation
+        self.__time_settings.paused = True
+
+    def _on_hold(self):
+        self.screen_position = pygame.Vector2(pygame.mouse.get_pos()) - self.__mouse_offset_from_body
+
+    def _on_hold_end(self):
+        self.__mouse_offset_from_body = None
+
     def _on_click(self):
         if self.is_selected:
             return
@@ -83,13 +121,17 @@ class BodyController(CircleClickable):
         """
         The method that's called when the body gets selected
         """
-        print(f"{self.sim_object.tag} selected")
+        # print("selected")
+        self.__selection_renderer.is_active = True
+        self.__vector_renderer.is_active = True
 
     def _on_deselect(self):
         """
         The method that's called when the body is deselected
         """
-        print(f"{self.sim_object.tag} deselected")
+        # print("deselected")
+        self.__selection_renderer.is_active = False
+        self.__vector_renderer.is_active = False
 
     def _clickable_end(self):
         self.sim_object.environment.event_system.remove_listener(SelectionUpdateEvent, self.__handle_selection_update)
