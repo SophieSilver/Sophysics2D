57a9257e6cdb35cebe45083843c425385d9e01b8
SophieSilver <87021304+SophieSilver@users.noreply.github.com>
2022-01-09 21:53:01 +0300

    Added attraction manager, global selection and velocity controller to setup_env.py

--- application/setup_env.py
+++ application/setup_env.py
@@ -3,8 +3,10 @@ import pygame_gui
 
 from sophysics_engine import SimEnvironment, TimeSettings, PhysicsManager, \
     Camera, GUIManager, PygameEventProcessor
-from defaults import CameraController, PauseOnSpacebar
+from defaults import CameraController, PauseOnSpacebar, AttractionManager
 from .lower_panel import LowerPanel
+from .selection import GlobalSelection
+from .velocity_controller import VelocityController
 from typing import Dict
 
 
@@ -22,12 +24,32 @@ def get_environment_from_config(display: pygame.Surface, config: Dict) -> SimEnv
     gui_manager_component = GUIManager(pygame_ui_manager, **config["GUIManagerComponentArgs"])
     event_processor = PygameEventProcessor()
     # can't store rect in a json, so we have to do this awkwardness
+    cam_controller_config = config["cameraControllerCfg"]
     camera_controller = CameraController(
         camera,
-        pygame.Rect(config["cameraControllerCfg"]["rect"]),
-        config["cameraControllerCfg"]["hold_time"],
-        config["cameraControllerCfg"]["zoom_strength"],
-        config["cameraControllerCfg"]["min_camera_scale"])
+        pygame.Rect(cam_controller_config["rect"]),
+        cam_controller_config["hold_time"],
+        cam_controller_config["zoom_strength"],
+        cam_controller_config["min_camera_scale"]
+    )
+
+    attraction_manager = AttractionManager(config["attractionCfg"]["attraction_coefficient"])
+    
+    selection_config = config["globalSelectionCfg"]
+    global_selection = GlobalSelection(
+        rect=pygame.Rect(selection_config["rect"]),
+        button=selection_config["button"],
+        hold_time=selection_config["hold_time"]
+    )
+
+    vel_controller_config = config["velocityControllerCfg"]
+    vel_controller = VelocityController(
+        camera=camera,
+        rect=pygame.Rect(vel_controller_config["rect"]),
+        scale_factor=vel_controller_config["scale_factor"],
+        button=vel_controller_config["button"],
+        hold_time=vel_controller_config["hold_time"]
+    )
 
     time_control_panel = LowerPanel(config["timeControlPanelCfg"])
 
@@ -35,7 +57,8 @@ def get_environment_from_config(display: pygame.Surface, config: Dict) -> SimEnv
 
     env = SimEnvironment((), (
         time_settings, physics_manager, camera, gui_manager_component,
-        event_processor, time_control_panel, camera_controller, pause_on_spacebar
+        event_processor, time_control_panel, camera_controller, pause_on_spacebar,
+        attraction_manager, global_selection, vel_controller
     ))
 
     return env
